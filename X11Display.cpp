452f7eb0567a5eb09aaaf7b0a89b547409ee4a69
Claudio Dettoni <cd611@cl.cam.ac.uk>
2025-02-25 19:27:57 +0000

    cleanup

--- X11Display.cpp
+++ X11Display.cpp
@@ -4,9 +4,7 @@
 #include "X11Display.hpp"
 
 X11Display::X11Display(const std::string& name) {
-    std::cout << "LOL" << std::endl;
     display = XOpenDisplay(name.c_str());
-    std::cout << "LL" << std::endl;
     if (display == NULL) {
         throw std::runtime_error("Display not found");
     }
--- render.cpp
+++ render.cpp
@@ -10,11 +10,6 @@
 const float  PI_F=3.14159265358979f;
 
 int main() {
-    //Obj o("/mnt/c/Users/sauron/Documents/untitled.obj");
-    Point<float> p(1,1,1);
-    Transform t = Transform::translate(1.,1.,0)*Transform::rotX(PI_F/2);
-    auto p2 = p*t;
-
     X11Display d("");
     X11Window w = X11Window::CreateSimpleWindow(&d, 640, 640);
     d.map_window(w);      
@@ -58,30 +53,17 @@ int main() {
         }
         frame.fill(Color::Black);
 
-        Point<float> t[3] = {Point<float> (0, 0, -100), Point<float> (0, 50, -100), Point<float> (50,0,100)};
-        int off = 50.0*std::sin(i/30.);
-        Point<float> tt[3] = {Point<float> (off, 0, 0), Point<float> (50+off, 50, 0), Point<float> (50+off,0,0)};
-        //frame.draw_triangle(tt[0], tt[1], tt[2], Color::Blue);
-        //frame.draw_triangle(t[0], t[1], t[2], Color::Red);
-        //frame.draw_triangle(t0[0], t0[1], t0[2], Color::Red);
-        //frame.draw_triangle(t1[0], t1[1], t1[2], Color::White);
-        //frame.draw_triangle(t2[0], t2[1], t2[2], Color::Green);
-        //frame.draw_line(13, 20, 80, 40, Color::Black);
-        //frame.draw_line(20, 13, 40, 80, Color::Red);
-        //frame.draw_line(80, 40, 13, 20, Color::Red);
-        //frame.draw_line(50, 50, 80, 75, Color::Blue);
-        //frame.draw_line(80, 75, 130, 75, Color::Blue);
-        //frame.draw_line(130, 75, 100, 50, Color::Blue);
-        //frame.draw_line(100, 50, 50, 50, Color::Blue);
-        //frame.draw_line(80, 75, 100, 50, Color::Blue);
-        Transform tr = Transform::scale(300,300,300) * Transform::translate(1.,1.,0) * Transform::rotY(i*0.05);
-        r.render(frame, tr);
+        Transform transform = Transform::scale(300,300,300) * Transform::translate(1.,1.,0) * Transform::rotY(i*0.05);
+        r.render(frame, transform);
         w.update();
-        if (i % 10 == 0) {
+
+        int framesSample = 50;
+        if (i % framesSample == 0) {
             clock_t end = clock();
             unsigned long micros = end - start;
             start = clock();
-            printf("FPS: %d\n", (int)(10000000/micros));
+            int fps = framesSample * CLOCKS_PER_SEC / micros;
+            printf("FPS: %d\n", fps);
         }
     } 
 
